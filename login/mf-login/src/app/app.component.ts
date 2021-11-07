649f6b14aad6bb2549f38307900f029cbe937c95
JUARSO <juan09fut.js@gmail.com>
2021-11-06 22:30:59 -0600

    Login
    Se agrego una parte de la parte visual del login monolitico

--- login/mf-login/src/app/app.component.ts
+++ login/mf-login/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'mf-login',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'mf-login';
