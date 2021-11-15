33976fddb9b761e3596a68fbb3b8e8adbfed988b
JUARSO <juan09fut.js@gmail.com>
2021-11-14 18:11:00 -0600

    Login funcional visualmente

--- login/mf-login/src/app/app.component.ts
+++ login/mf-login/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'mf-login',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private router: Router) {}
+
+  public navegation(): void{
+    this.router.navigate(['/VistaPrincipal']);
+  }
+  
   title = 'mf-login';
 }
+
